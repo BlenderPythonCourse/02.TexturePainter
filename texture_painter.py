45f5591ef451aa59c886faacbbafb5d7e723c9fa
Ben Tristem <github@bentristem.com>
2016-11-02 11:22:42 +0000

    TP02 - Project Setup & GitHub

---
+++ texture_painter.py
@@ -0,0 +1,10 @@
+'''Renders text from a CSV file to textures
+and applies them to multiple objects.
+
+Use snippets...
+import os, sys; sys.path.append(os.path.dirname(bpy.data.filepath)); import texture_painter
+import importlib; importlib.reload(texture_painter); texture_painter.go()
+'''
+
+def go():
+    print("Texture Painter starting up.")

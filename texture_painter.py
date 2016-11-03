f9545e6a9bb81340fdec4ea54299946ca055076d
Ben Tristem <github@bentristem.com>
2016-11-03 19:42:09 +0000

    TP05 - Rendering Text To Images

--- texture_painter.py
+++ texture_painter.py
@@ -8,6 +8,7 @@ import importlib; importlib.reload(texture_painter); texture_painter.go()
 
 import codecs
 import csv
+from PIL import Image, ImageFont, ImageDraw
 
 def get_backers(csv_filename):
     with codecs.open(csv_filename, 'r', 'utf-8-sig') as stream:
@@ -17,8 +18,16 @@ def get_backers(csv_filename):
             backer = dict(zip(header, row))
             yield backer
 
+# image size and font-size hard-coded in method
+def render_text_to_file(text_to_render):
+    image = Image.new('RGB', (128,128))
+    draw = ImageDraw.Draw(image)
+    fnt = ImageFont.truetype('arial.ttf', 50)
+    draw.text((0,0), text_to_render, font=fnt, fill=(255,255,255))
+    image.save("test.png")
+
 def go():
     print("Texture Painter starting up.")
     # Read through the CSV
     for backer in get_backers('backers_10.csv'):
-        print(backer)
+        render_text_to_file("It worked!")
